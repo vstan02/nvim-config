60e82f940eb313dba812c67bb5f98dafb75cfd7f
Stan Vlad <vstan02@protonmail.com>
2024-03-29 16:36:47 +0200

    Initial commit.

---
+++ lua/configs/conform.lua
@@ -0,0 +1,11 @@
+local options = {
+  formatters_by_ft = {
+    lua = { 'stylua' },
+  },
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+}
+
+require('conform').setup(options)
---
+++ lua/configs/lspconfig.lua
@@ -0,0 +1,23 @@
+local on_attach = require('nvchad.configs.lspconfig').on_attach
+local on_init = require('nvchad.configs.lspconfig').on_init
+local capabilities = require('nvchad.configs.lspconfig').capabilities
+
+local lspconfig = require('lspconfig')
+local servers = {
+  'clangd',
+  'cmake',
+  'clojure_lsp',
+  'bashls',
+  'cssls',
+  'somesass_ls',
+  'html',
+  'tsserver',
+}
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup({
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+  })
+end
